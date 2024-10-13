a8e4e0edec5c98c9f11dd141ead36af993e76a45
chu thanh tung <tungchuthanh1234@gmail.com>
2024-10-13 14:28:46 +0700

    add-route-controller-service for suspend student

--- src/routes/initWebRoutes.ts
+++ src/routes/initWebRoutes.ts
@@ -3,6 +3,8 @@ import { Router, Express } from 'express'
 import { registerStudentsController } from '@controllers/teacherController/registerStudents'
 
 import { getCommonStudentsController } from '@controllers/teacherController/getCommonStudents'
+
+import { suspendStudentController } from '@controllers/teacherController/suspendStudent'
 const initWebRoutes = (app: Express) => {
   // Create a base router to handle all routes under `/api`
   const apiRouter = Router()
@@ -14,6 +16,8 @@ const initWebRoutes = (app: Express) => {
   apiRouter.post('/register', registerStudentsController)
 
   apiRouter.get('/commonstudents', getCommonStudentsController)
+
+  apiRouter.post('/suspend', suspendStudentController)
 
   // Apply the `/api` prefix to all routes
   app.use('/api', apiRouter)
---
+++ src/services/teacherService/suspendStudent.ts
@@ -0,0 +1,14 @@
+import { globalErrCode } from '@constants/errorCode'
+import { Student } from '@models/Student'
+import { HttpException } from '@utils/httpException'
+
+export const suspendStudentService = async (studentEmail: string): Promise<void> => {
+  const student = await Student.findByPk(studentEmail)
+
+  if (!student) {
+    throw new HttpException(globalErrCode['STUDENT.0002'])
+  }
+
+  student.suspended = true
+  await student.save()
+}
