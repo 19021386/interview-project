0a8995f088ab03a64e949b8aecf0b4b115e8f0ba
chu thanh tung <tungchuthanh1234@gmail.com>
2024-10-14 10:18:39 +0700

    reuse log middleware logic, update register from set to add

--- src/bin/dev.ts
+++ src/bin/dev.ts
@@ -1,35 +1,28 @@
-/**
- * Module dependencies.
- */
-function dev() {}
-
-import app from '../app' // Assuming the app module is using ES6 export
-// import debug from 'debug'; // You can uncomment this line if using 'debug'
+import app from '../app'
 import http from 'http'
+function dev() {
+  const port = normalizePort(process.env.PORT || '3000')
+  app.set('port', port)
 
-const port = normalizePort(process.env.PORT || '3000')
-app.set('port', port)
+  const server = http.createServer(app)
 
-const server = http.createServer(app)
+  server.listen(port, () => {
+    console.log(`Server listening on port: ${port}`)
+  })
 
-server.listen(port, () => {
-  console.log(`Server listening on port: ${port}`)
-})
+  function normalizePort(val: string) {
+    const port = parseInt(val, 10)
 
-function normalizePort(val: string) {
-  const port = parseInt(val, 10)
+    if (isNaN(port)) {
+      return val
+    }
 
-  if (isNaN(port)) {
-    // named pipe
-    return val
+    if (port >= 0) {
+      return port
+    }
+
+    return false
   }
-
-  if (port >= 0) {
-    // port number
-    return port
-  }
-
-  return false
 }
 
 dev()
--- src/middleware/errorLogger.ts
+++ src/middleware/errorLogger.ts
@@ -1,31 +1,10 @@
 /* eslint-disable prettier/prettier */
-import { format } from 'date-fns';
-import { v4 as uuid } from 'uuid';
-
 import { Request, Response, NextFunction } from 'express'
 
 import { Error } from '@constants/interface'
 
-import fs from 'fs';
-import { promises as fsPromises } from 'fs';
-import path from 'path';
-
-export const eventLogger = async (error: Error, message: string, logName: string, next: NextFunction) => {
-    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`
-    const logItem = `${dateTime}\t${uuid()}\t${message}\t${error}\n`
-
-    try {
-        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
-            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'))
-        }
-
-        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logName), logItem)
-    } catch (err) {
-        next(err)
-    }
-}
+import { eventLogger } from '@utils/eventLogger';
 
 export const errorLogger = (error: Error, req: Request, res: Response, next: NextFunction) => {
-    eventLogger(error, `${req.method}\t${req.headers.origin}\t${req.url}`, 'errLog.txt', next)
-    // console.log(`${req.method} ${req.url}`)
+    eventLogger( `${req.method}\t${req.headers.origin}\t${req.url}`, 'errLog.txt', next, error)
 }
